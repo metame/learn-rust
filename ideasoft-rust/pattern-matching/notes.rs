51ec8c97752100ea2fa6c7bd3f1546bfa0e1c216
Michael J Erwin <michaeljameserwin@gmail.com>
2023-03-17 16:40:42 -0400

    pattern matching

---
+++ ideasoft-rust/pattern-matching/notes.rs
@@ -0,0 +1,84 @@
+fn main() {
+    enum Snack {
+        Pretzels { filled: bool },
+        Chips { ounces: f64 },
+    }
+
+    let snack = Snack::Pretzels { filled: true };
+    // let Snack::Pretzels { filled } = snack; // error[E0005]: refutable pattern in local binding
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^^^ pattern `Snack::Chips { .. }` not covered
+
+    if let Snack::Pretzels { .. } = snack {
+        println!("I'm having pretzels!");
+    }
+
+    // interestingly, the course showed how specifying `filled` above and not using it would result in compiler warning
+    // but they didn't actually demonstrate that we were creating a scoped variable to the inner block... missed opportunity there
+    if let Snack::Pretzels { filled } = snack {
+        println!("I'm having{} pretzels!", if filled {" filled"} else {""});
+    }
+
+    // println!("filled is {}", filled); // error[E0425]: cannot find value `filled` in this scope
+
+    if let Snack::Pretzels { filled: true } = snack {
+        println!("I'm having filled pretzels");
+    }
+
+
+    // another example of unneccessarily confusing people
+    // original example
+    for n in 0..=5 {
+        match n {
+            1 => println!("It was one!"),
+            2 => println!("It was two!"),
+            // or-pattern
+            3 | 4 => println!("It was a bit more than two!"),
+            // match guard
+            high if high >= 5 => println!("It was a high number!"),
+            // a pattern consisting only of the name `other`
+            other => println!("It was {other}!"),
+        }
+    }
+
+    // clearer example (e.g. the match guard and actually showing the default)
+    for n in 0..=7 {
+        match n {
+            1 => println!("It was one!"),
+            2 => println!("It was two!"),
+            // or-pattern
+            3 | 4 => println!("It was a bit more than two!"),
+            // match guard
+            high if high >= 5 && high < 7 => println!("{high} is a high number!"),
+            // a pattern consisting only of the name `other`
+            other => println!("It was {other}!"),
+        }
+    }
+
+
+    match snack {
+        Snack::Pretzels { filled } if filled => {
+            println!("I had filled pretzels!");
+        }
+        Snack::Pretzels { .. } => {
+            println!("I had pretzels!")
+        }
+        Snack::Chips { .. } => todo!(),
+    }
+
+    let mut microwaved_snack = Snack::Pretzels { filled: false };
+
+    while let Snack::Pretzels { .. } = microwaved_snack {
+        println!("Having pretzels!");
+        microwaved_snack = Snack::Chips { ounces: 12.0 };
+    }
+    println!("Finally eating chips!");
+
+    enum Key { Up, Down, Left, Right }
+
+    match Key::Left {
+        Key::Up => println!("Jumping"),
+        Key::Down => println!("Ducking!"),
+        Key::Left => println!("Sliding Left!"),
+        Key::Right => println!("Sliding Right!"),
+    }
+}
