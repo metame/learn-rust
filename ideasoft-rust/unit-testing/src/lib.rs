d272f8ac12ef649b214bdb028120400780d701c1
Michael J Erwin <michaeljameserwin@gmail.com>
2023-07-17 09:59:02 -0400

    unit-testing

---
+++ ideasoft-rust/unit-testing/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn adding_unequal_numbers_works() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
