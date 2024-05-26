a0f4fc6095c038322ce1aa247e7c1f9bf6aac9b5
Michael J Erwin <michaeljameserwin@gmail.com>
2024-05-26 11:27:21 -0400

    rust in action: chip 8 complete

--- rust-in-action/snippets/chip8.rs
+++ rust-in-action/snippets/chip8.rs
@@ -26,9 +26,15 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+            // this was just me playing with bit manipulatin instead of using the mask
+            // which is how I originally implemented `call` before reading the book's solution
+            let a = ((x as u16) << 8) | ((y as u16) << 4) | (d as u16);
+            assert_eq!(nnn, a);
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => { return; },
-                (2, _, _, _) => self.call(x, y, d),
+                (2, _, _, _) => self.call(nnn),
                 (0, 0, 0xE, 0xE) => self.call_return(),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {opcode:04x}"),
@@ -36,13 +42,26 @@ impl CPU {
         }
     }
 
-    fn call(&mut self, x: u8, y: u8, d: u8) {
-        let a = ((x as usize) << 12) | ((y as usize) << 8) | (d as usize);
-        self.position_in_memory = a;
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!");
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
     }
 
     fn call_return(&mut self) {
-        todo!();
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow!");
+        }
+
+        self.stack_pointer -= 1;
+        self.position_in_memory = self.stack[self.stack_pointer] as usize;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -67,22 +86,24 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80; mem[1] = 0x14;
-    mem[2] = 0x80; mem[3] = 0x24;
-    mem[4] = 0x80; mem[5] = 0x34;
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
-
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
-
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
